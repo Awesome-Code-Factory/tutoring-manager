d8c53b035d02866d1bb7e9774535d3b07513e8c4
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2024-09-03 21:08:40 +0200

    feat: add few components and basic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-import { Button } from "@/components/ui/button";
+import { PreviewButton } from "@/components/preview-components";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to the page!</h1>
-      <Button>Click me</Button>
+    <main className="mx-auto flex max-w-5xl flex-col gap-12 p-5">
+      <section className="flex flex-col gap-4">
+        <h1>Welcome to the page!</h1>
+        <div>
+          <PreviewButton />
+        </div>
+      </section>
+      <section>
+        <h2>Here is the main page</h2>
+        <div>
+          <p>this section shows h2 header with a paragraph</p>
+          <p>This is purely to showcase shadcn/ui components</p>
+        </div>
+      </section>
     </main>
   );
 }
---
+++ src/components/user-button.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { LogOut, Moon, Sun } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useTheme } from "next-themes";
+import { toast } from "sonner";
+
+export const UserButton = () => {
+  const { setTheme, theme } = useTheme();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="absolute right-5 top-5">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            {theme === "dark" ? (
+              <Sun className="mr-2 h-4 w-4" />
+            ) : (
+              <Moon className="mr-2 h-4 w-4" />
+            )}
+            <span>Change Theme</span>
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuItem onClick={() => toast.error("You can't log out, LOL")}>
+          <LogOut className="mr-2 h-4 w-4" />
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
